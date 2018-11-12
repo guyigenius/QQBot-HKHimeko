1f72cc84988b5d988a2b2b653eb691a872c7aa77
guyigenius <317863605@qq.com>
2018-11-12 17:05:44 +0800

    更改CD为每30分钟2张（原来是每20分钟1张）。 添加色图的稀有度（来自群友建议）。

--- Newbe.Mahua.Plugins.HKHimeko/MahuaEvents/GroupMessageReceivedMahuaEventNeedPorn.cs
+++ Newbe.Mahua.Plugins.HKHimeko/MahuaEvents/GroupMessageReceivedMahuaEventNeedPorn.cs
@@ -23,12 +23,11 @@ namespace Newbe.Mahua.Plugins.HKHimeko.MahuaEvents
 
         private class Status /*记录每位群友的色图统计状态*/
         {
-            public DateTime dateTime; /*上次我要色图的时间*/
+            public Queue<DateTime> dateTimeQueue; /*最近两次我要色图的时间*/
             public int used; /*已经使用的限额*/
-            public int totalScore; /*当前抽到的色图的总得分，即使怀旧色图平均分必然比今日色图高不少，我还是没有给今日色图加权，因为我觉得现在这么设置的话不太可能进入二阶段了，如果能够进入那就当做奖励吧*/
+            public int totalScore; /*当前抽到的色图的总分，即使怀旧色图平均分必然比今日色图高不少，我还是没有给今日色图加权，因为我觉得现在这么设置的话不太可能进入二阶段了，如果能够进入那就当做奖励吧，也不考虑惩罚分了。*/
             public double averageScore; /*当前抽到的色图的平均分*/
             public bool buff; /*是否获得了港姬子的Super AI Buff*/
-            //public int finalScore; /*当前包括惩罚的最终得分，最终得分公式没有想好*/
         };
 
         private static JArray jArr = new JArray(); /*query返回的JSON*/
@@ -37,9 +36,12 @@ namespace Newbe.Mahua.Plugins.HKHimeko.MahuaEvents
         private static int[] pornArray = null; /*决定发送顺序的随机数组*/
         private static int pornIndex = 0; /*记录当前将要发送的数组索引*/
         private static bool updateLock = false; /*更新的锁*/
-        //private static bool sendLock = false; /*发送的锁*/
         private static HashSet<int> record = new HashSet<int>(); /*记录发送过的图片id的哈希集*/
         private static Dictionary<string, Status> status = new Dictionary<string, Status>();  /*不加限额根本扛不住*/
+        // 每人每日限额12张，CD每30分钟2张。Super AI Buff：+100%限额，-90%冷却缩减。
+        private readonly int quota = 12;
+        private readonly int cooldown = 30;
+        private readonly int quotaInCooldown = 2;
         private static string firstQq = "";
         //private static double[] rankingAverageScore = new double[3] { 0.0, 0.0, 0.0 };
         private static double firstAverageScore = 0.0;
@@ -159,11 +161,11 @@ namespace Newbe.Mahua.Plugins.HKHimeko.MahuaEvents
                 {
                     _mahuaApi.SendGroupMessage(context.FromGroup)
                     .At(firstQq)
-                    .Text($"恭喜！由于你昨天得分第一，你今天一整天都会获得港姬子的Super AI Buff（+100%限额，-90%冷却缩减）哦！")
+                    .Text($" 恭喜！由于你昨天得分第一，你今天一整天都会获得港姬子的Super AI Buff（+100%限额，-90%冷却缩减）哦！")
                     .Done();
                     status[firstQq] = new Status
                     {
-                        dateTime = new DateTime(),
+                        dateTimeQueue = new Queue<DateTime>(),
                         used = 0,
                         totalScore = 0,
                         averageScore = 0.0,
@@ -232,19 +234,19 @@ namespace Newbe.Mahua.Plugins.HKHimeko.MahuaEvents
                 pornIndex = 0;
             }
 
-            // 每人每日限额12张，CD20分钟。Super AI Buff：+100%限额，-90%冷却缩减。
             if (status.ContainsKey(context.FromQq))
             {
-                if (status[context.FromQq].used < (status[context.FromQq].buff ? 24 : 12))
+                if (status[context.FromQq].used < (status[context.FromQq].buff ? 2 * quota : quota))
                 {
-                    if (DateTime.Now.Subtract(status[context.FromQq].dateTime).TotalMinutes < (status[context.FromQq].buff ? 2 : 20))
+                    if (DateTime.Now.Subtract(status[context.FromQq].dateTimeQueue.Peek()).TotalMinutes < (status[context.FromQq].buff ? 0.1 * cooldown : cooldown) &&
+                        status[context.FromQq].dateTimeQueue.Count >= quotaInCooldown)
                     {
                         GetStatus(context);
                         return;
                     }
                     else
                     {
-                        status[context.FromQq].dateTime = DateTime.Now;
+                        status[context.FromQq].dateTimeQueue.Enqueue(DateTime.Now);
                         status[context.FromQq].used++;
                         status[context.FromQq].totalScore += jArr[pornArray[pornIndex]]["score"].ToObject<int>();
                         status[context.FromQq].averageScore = Math.Round((double)status[context.FromQq].totalScore / status[context.FromQq].used, 2);
@@ -252,6 +254,10 @@ namespace Newbe.Mahua.Plugins.HKHimeko.MahuaEvents
                         {
                             firstAverageScore = status[context.FromQq].averageScore;
                             firstQq = context.FromQq;
+                        }
+                        if (status[context.FromQq].dateTimeQueue.Count > quotaInCooldown)
+                        {
+                            status[context.FromQq].dateTimeQueue.Dequeue();
                         }
                     }
                 }
@@ -269,7 +275,7 @@ namespace Newbe.Mahua.Plugins.HKHimeko.MahuaEvents
             {
                 status[context.FromQq] = new Status
                 {
-                    dateTime = DateTime.Now,
+                    dateTimeQueue = new Queue<DateTime>(new DateTime[] { DateTime.Now }),
                     used = 1,
                     totalScore = jArr[pornArray[pornIndex]]["score"].ToObject<int>(),
                     averageScore = Math.Round(jArr[pornArray[pornIndex]]["score"].ToObject<double>(), 2),
@@ -282,7 +288,28 @@ namespace Newbe.Mahua.Plugins.HKHimeko.MahuaEvents
                 }
             }
 
-            //sendLock = true;
+            string rarity;
+            if (jArr[pornArray[pornIndex]]["score"].ToObject<int>() > 100)
+            {
+                rarity = "UR";
+            }
+            else if (jArr[pornArray[pornIndex]]["score"].ToObject<int>() > 75)
+            {
+                rarity = "SSR";
+            }
+            else if (jArr[pornArray[pornIndex]]["score"].ToObject<int>() > 50)
+            {
+                rarity = "SR";
+            }
+            else if (jArr[pornArray[pornIndex]]["score"].ToObject<int>() > 25)
+            {
+                rarity = "R";
+            }
+            else
+            {
+                rarity = "N";
+            }
+
             // 暂时框架对于发图功能支持有问题，会直接发送图片地址而不是图片。
             //string msg = $"[IR:pic={new DirectoryInfo("./AppData/Images/").FullName}{pornArray[pornIndex]}.jpg]\r\nhttps://yande.re/post/show/{jArr[pornArray[pornIndex]]["id"]}";
             //_mahuaApi.SendGroupMessage(context.FromGroup)
@@ -292,16 +319,27 @@ namespace Newbe.Mahua.Plugins.HKHimeko.MahuaEvents
             //    .Done();
             // 只能自己手动拼接字符串构造，注意因为Api_UpLoadPic上传的图片尺寸应小于4MB，否则会上传失败返回空，所以这里采用sample_url而不是jpeg_url。
             // 我怀疑因为之前只发一张图片和对应地址导致被腾讯认为是广告机所以总是被屏蔽群消息发送不出去，尝试添加Score和Tags信息来解决这个问题。
+            // 我现在进一步怀疑是因为yande.re这个网站被腾讯认为不安全所以总是被屏蔽群消息发送不出去，yande.re现在经常被墙大家也很少会去点开，因此尝试去掉网址直接使用id来解决这个问题。
+            // 最后尝试添加表情和Emoji来解决这个问题。
+            // 通过多次尝试得出结论：带有不安全链接/消息长度过长/消息内容雷同/图片太色都会被腾讯屏蔽，前两个我已经尽力避免了，第三个手动发都发不出去了更别提QQ机器人发了，无解。总之现在应该把被屏蔽的概率降到最低了。
             string msg = $"[IR:pic={jArr[pornArray[pornIndex]]["sample_url"]}]\r\n" +
-                // 尝试添加表情和Emoji。
                 $"[IR:at={context.FromQq}]\r\n" +
                 $"Id: [emoji=F09F9189]{jArr[pornArray[pornIndex]]["id"]}[emoji=F09F9188]\r\n" +
-                $"Score: [Face175.gif]{jArr[pornArray[pornIndex]]["score"]}[Face175.gif]";
-                // 太长的Tags也可能会导致被屏蔽。
-                //$"Tags: {jArr[pornArray[pornIndex]]["tags"]}";
-                // 我现在进一步怀疑是因为yande.re这个网站被腾讯认为不安全所以总是被屏蔽群消息发送不出去，yande.re现在经常被墙大家也很少会去点开，因此尝试去掉网址直接使用id来解决这个问题。
-                //$"https://yande.re/post/show/{jArr[pornArray[pornIndex]]["id"]}";
+                $"Tags: {jArr[pornArray[pornIndex]]["tags"]}\r\n" +
+                $"Score: {jArr[pornArray[pornIndex]]["score"]}\r\n" +
+                $"Rarity: [Face175.gif]{rarity}[Face175.gif]";
             _mahuaApi.SendGroupMessage(context.FromGroup, msg);
+
+            if (rarity == "UR")
+            {
+                status[context.FromQq].buff = true;
+                _mahuaApi.SendGroupMessage(context.FromGroup)
+                    .Text("ハラショー！")
+                    .Newline()
+                    .At(context.FromQq)
+                    .Text($" 恭喜！由于抽出了UR，你一整天都会获得港姬子的Super AI Buff（+100%限额，-90%冷却缩减）哦！")
+                    .Done();
+            }
 
             // 一个小彩蛋：Uploader是群主，触发概率大概是4%。
             if (jArr[pornArray[pornIndex]]["author"].ToString() == "fireattack")
@@ -317,10 +355,6 @@ namespace Newbe.Mahua.Plugins.HKHimeko.MahuaEvents
 
             record.Add(jArr[pornArray[pornIndex]]["id"].ToObject<int>());
             pornIndex++;
-
-            // 尝试通过强行睡上5秒的方式来避免CleverQQ虽然请求发送群消息了但是实际并没有成功发送群消息，也没有提示发送失败或者被屏蔽。
-            //Thread.Sleep(5000);
-            //sendLock = false;
         }
 
         /// <summary>
@@ -331,12 +365,25 @@ namespace Newbe.Mahua.Plugins.HKHimeko.MahuaEvents
         {
             if (status.ContainsKey(context.FromQq))
             {
-                _mahuaApi.SendGroupMessage(context.FromGroup)
+                if (DateTime.Now.Subtract(status[context.FromQq].dateTimeQueue.Peek()).TotalMinutes < (status[context.FromQq].buff ? 0.1 * cooldown : cooldown) &&
+                        status[context.FromQq].dateTimeQueue.Count >= quotaInCooldown)
+                {
+                    _mahuaApi.SendGroupMessage(context.FromGroup)
                     .At(context.FromQq)
-                    .Text($" 你刚刚才要过色图哦，等{(status[context.FromQq].buff ? 2 : 20) - DateTime.Now.Subtract(status[context.FromQq].dateTime).Minutes}分钟再试试看吧！")
+                    .Text($" 你刚刚才要过色图哦，等{(status[context.FromQq].buff ? 0.1 * cooldown : cooldown) - DateTime.Now.Subtract(status[context.FromQq].dateTimeQueue.Peek()).Minutes}分钟再试试看吧！")
                     .Newline()
-                    .Text($"你今天已经要过{status[context.FromQq].used}张色图了，当前得分是{status[context.FromQq].averageScore}。")
+                    .Text($"你今天已经要过{status[context.FromQq].used}张色图了，当前平均分是{status[context.FromQq].averageScore}。")
                     .Done();
+                }
+                else
+                {
+                    _mahuaApi.SendGroupMessage(context.FromGroup)
+                    .At(context.FromQq)
+                    .Text($" 你现在就可以要色图哦！")
+                    .Newline()
+                    .Text($"你今天已经要过{status[context.FromQq].used}张色图了，当前平均分是{status[context.FromQq].averageScore}。")
+                    .Done();
+                }
             }
             else
             {
@@ -355,7 +402,7 @@ namespace Newbe.Mahua.Plugins.HKHimeko.MahuaEvents
         {
             _mahuaApi.SendGroupMessage(context.FromGroup)
                 .At(context.FromQq)
-                .Text($" 贵群当前最高得分为{firstAverageScore}，请继续努力哦！")
+                .Text($" 贵群当前最高分为{firstAverageScore}，请继续努力哦！")
                 .Done();
         }
 
@@ -374,12 +421,6 @@ namespace Newbe.Mahua.Plugins.HKHimeko.MahuaEvents
                         .Done();
                     Thread.Sleep(10000);
                 }
-                // 一般来说10秒肯定可以把json接收完了，这里以防万一。
-                //while (updateLock || sendLock)
-                //while (updateLock)
-                //{
-                //    Thread.Sleep(5000);
-                //}
                 NeedPorn(context);
             }
             if (context.Message.Equals("我要色图 状态"))
